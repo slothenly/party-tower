2b88279192659807f5a338ac61b293e59bb8d905
Beatrice <Beatrice@DESKTOP-FOGA9V5>
2018-05-07 22:01:51 -0400

    Barebones set up for left panels, main panel still to go

--- External Level Designer Tool/External Level Designer Tool/Form1.cs
+++ External Level Designer Tool/External Level Designer Tool/Form1.cs
@@ -47,15 +47,18 @@ namespace External_Level_Designer_Tool
             this.Controls.Add(rightP);
             #endregion
 
-//  #############################################################################
+            //  #############################################################################
 
             #region Left Panel Specifics
 
-            //Selector Panel and Button Panel
+            //****************************************************
+            //****************** Panel Creation ******************
+            //****************************************************
+
             Panel typeSelector = new Panel();
             typeSelector.Height = 7 * (leftP.Height / 10);
             typeSelector.Width = leftP.Width;
-            typeSelector.BackColor = Color.Bisque;
+            typeSelector.BackColor = Color.Gray;
             typeSelector.AutoScroll = true;
 
             Panel btnSelector = new Panel();
@@ -64,10 +67,11 @@ namespace External_Level_Designer_Tool
             btnSelector.Width = leftP.Width;
             btnSelector.BackColor = Color.LightSeaGreen;
 
+
             //****************************************************
+            //************** Button Panel Buttons ****************
             //****************************************************
 
-            //Button Panel Buttons
             Button btnPlace = new Button();
             btnPlace.Width = leftP.Width / 2;
             btnPlace.Height = 2 * (btnSelector.Height / 7);
@@ -103,7 +107,9 @@ namespace External_Level_Designer_Tool
             btnExport.BackColor = Color.BurlyWood;
             btnExport.Text = "Export";
 
+
             //****************************************************
+            //**************** Adding to Controls ****************
             //****************************************************
 
             //Adding everything to controls
@@ -115,15 +121,26 @@ namespace External_Level_Designer_Tool
             btnSelector.Controls.Add(fileName);
             btnSelector.Controls.Add(btnImport);
             btnSelector.Controls.Add(btnExport);
+
+            //8 Characters per line
+            AddNewTile("dirt", "Glow Stone", typeSelector);
+            AddNewTile("brick", "Brick", typeSelector);
+            AddNewTile("grass", "Grass", typeSelector);
+            AddNewTile("dirt", "Glow Stone", typeSelector);                //            <=== Add Tilesets here
+            AddNewTile("brick", "WWWWWW", typeSelector);
+            AddNewTile("grass", "WWWWWWW", typeSelector);
+            AddNewTile("dirt", "WWWWWWWW", typeSelector);
+            AddNewTile("brick", "WWWWWWWW", typeSelector);
+            AddNewTile("grass", "Grass", typeSelector);
             #endregion
 
-//  #############################################################################
+            //  #############################################################################
 
             #region Right Panel Specifics
 
             #endregion
 
-//  #############################################################################
+            //  #############################################################################
 
         }
 
@@ -131,13 +148,53 @@ namespace External_Level_Designer_Tool
 
         #region Helper Functions
 
-        private void AddNewTile(string tilePath)
+            /// <summary>
+            /// Simplified way to add a new tile to tile list and to parent panel
+            /// </summary>
+            /// <param name="tilePath"></param>
+        private void AddNewTile(string tilePath, string tileSetName, Panel parent)
         {
-            ImageBox temp = new ImageBox();
-            temp.Height = 30;
-            temp.Width = 30;
-            temp.Top = 50 * Selectables.Count;
-            
+            ImageBox tempImageBox = new ImageBox();
+            tempImageBox.Height = 64;
+            tempImageBox.Width = 64;
+            tempImageBox.Top = 10 + (74 * Selectables.Count);
+            tempImageBox.Left = 10;
+            tempImageBox.Image = ImageSelect(tilePath);
+            tempImageBox.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            tempImageBox.SizeMode = PictureBoxSizeMode.Zoom;
+
+            Label tempLabel = new Label();
+            tempLabel.Left = 30 + tempImageBox.Width;
+            tempLabel.Top = 30 + (74 * Selectables.Count);
+            tempLabel.Height = tempImageBox.Height - 10;
+            tempLabel.Width = parent.Width - tempImageBox.Width - 50;
+            tempLabel.Text = tileSetName;
+            tempLabel.ForeColor = Color.White;
+            tempLabel.Font = new Font("Coder's Crux", 35, FontStyle.Regular);
+
+            parent.Controls.Add(tempImageBox);
+            parent.Controls.Add(tempLabel);
+            Selectables.Add(tempImageBox);
+
+        }
+
+        /// <summary>
+        /// Selects and image from the resources folder based on the input string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private Image ImageSelect(string s)
+        {
+            Image temp = null;
+            try
+            {
+                temp = Image.FromFile(@"..\..\Resources\" + s + ".png");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Selected tile does not exist." + Environment.NewLine + e.Message);
+            }
+            return temp;
         }
 
         #endregion
