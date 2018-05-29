28ac77eca41d02031ccf34d3a26fdc86a48c28f5
Marco <jxm5725@rit.edu>
2018-05-29 03:24:11 -0400

    a* is now 2 person compatable, initated it in game, and added Enemy TYpes

--- Party Tower Main/Party Tower Main/PathManager.cs
+++ Party Tower Main/Party Tower Main/PathManager.cs
@@ -17,17 +17,44 @@ namespace Party_Tower_Main
         //fields ------------------------------------------------------------------------
         private List<Location> openList;            // List of unchecked nodes
         private List<Location> closedList;          // List of checked nodes - final path held in here
-        private string[] map;                       // Curent Array of Strings that depicts the map of the level
+        private Vector2 p1Center;                   // Center point of Player 1 that the enemy may need to move to.
+        private Vector2 p2Center;                   // Center point of Player 2 that the enemy may need to move to.
+        private Location p1Location;                // Cordinates of the tile that player1 rests in
+        private Location p2Location;                // Cordinates of the tile that player2 rests
+        private string[] p1Map;                     // Curent Array of Strings that depicts the map of the level with Player 1
+        private string[] p2Map;                     // Curent Array of Strings that depicts the map of the level with Player 2
+        private string[] correctMap;                // Field that gets set to either p1Map or p2Map based on what player the enemy is looking for. 
         private Location current;                   // Current Location being checked
         private Location start;                     // Starting Location - The location of the enemy during an update
         private Location target;                    // Target Location - The location of the Player during an update
+        private bool usePlayer1;                    // Bool that determines if the most recent player checked for this enemy was Player 1 or Player 2
+
         int g = 0;                                  
+
+        //tile constants to determine player and enemy positions on map
+        private int heightConstant;                 // based on minimum # of tiles it base resolution allows - height
+        private int widthConstant;                  // based on minimum # of tiles it base resolution allows - width
 
         //properties --------------------------------------------------------------------
 
-        public string[] MapOfLevel
+        public string[] P1MapOfLevel
         {
-            get { return map; }
+            get { return p1Map; }
+        }
+
+        public string[] P2MapOfLevel
+        {
+            get { return p1Map; }
+        }
+
+        public int HeightConstant
+        {
+            get { return heightConstant; }
+        }
+
+        public int WidthConstant
+        {
+            get { return widthConstant; }
         }
 
         /* Example of Map as String[]
@@ -44,25 +71,113 @@ namespace Party_Tower_Main
          */
 
         //constructor -------------------------------------------------------------------
-        public PathManager()
+        public PathManager(Viewport view)
         {
             openList = new List<Location>();
             closedList = new List<Location>();
-            map = new string[] { };
+            p1Map = new string[] { };
+            p2Map = new string[] { };
+            SetMapConstants(view.Height, view.Width);
         }
 
         //methods -----------------------------------------------------------------------
 
-        public void SetNewLevel(string[] newMap)
+        /// <summary>
+        /// Updates inner constants needed for calculations. Call if the user changes resolution sizes.
+        /// (IE: 1920 by 1080 to 1080 by 720, etc.)
+        /// </summary>
+        /// <param name="resolutionHeight">Current Screen Resolution Height</param>
+        /// <param name="resolutionWidth"> Current Screen Resolution Width</param>
+        public void SetMapConstants(int resolutionHeight, int resolutionWidth )
         {
-            map = newMap;
+            // Confirm this logic w/ Justin
+
+            // 9 tiles up / down loaded on screen at minimum
+            // 16 tiles left right loaded on screen at minimum.
+
+            // So, the logic is to divide the screen size by the total tiles allowed in each direction to get the tile constants of x and y
+            heightConstant = resolutionHeight / 9;
+            widthConstant = widthConstant / 16;
+            
         }
 
-
-        public void Following()
+        /// <summary>
+        /// Creates a map of the level for the enemy to follow for either player. 
+        /// Only needs called once per Update Loop, before checking enemys.
+        /// </summary>
+        /// <param name="newMap"> Map of the current level </param>
+        /// <param name="player1"> Hitbox of Player 1 </param>
+        /// <param name="player2"> Hitbox of Player 2 </param>
+        public void UpdatePlayersOnMap(string[] newMap, Rectangle player1, Rectangle player2)
         {
-            var start = new Location {};
-            var target = new Location {};
+            p1Center = new Vector2(player1.Center.X, player1.Center.Y);
+            p2Center = new Vector2(player2.Center.X, player2.Center.Y);
+
+            //Records the correct tile cordinates based on the location of the player
+            p1Location = new Location { X = (player1.Center.X / widthConstant), Y =  (player1.Center.Y / heightConstant) };
+            p2Location = new Location { X = (player2.Center.X / widthConstant), Y = (player2.Center.Y / heightConstant) };
+
+            //Creates new "original" maps for player1 and player2 to be set in
+            p1Map = newMap;
+            p2Map = newMap;
+
+            //Sets the respective char in the respective string within the string array to include Player 1
+            var p1mapLine = newMap[p1Location.Y];
+            var aStringBuilder = new StringBuilder(p1mapLine);
+            aStringBuilder.Remove(p1Location.X, 1);
+            aStringBuilder.Insert(p1Location.X, "P");
+            p1mapLine = aStringBuilder.ToString();
+
+            //Creates the map to find player 1 
+            p1Map[p1Location.Y] = p1mapLine;
+
+            //Sets the respective char in the respective string within the string array to include Player 2
+            var p2mapLine = newMap[p2Location.Y];
+            aStringBuilder = new StringBuilder(p2mapLine);
+            aStringBuilder.Remove(p2Location.X, 1);
+            aStringBuilder.Insert(p2Location.X, "P");
+            p2mapLine = aStringBuilder.ToString();
+
+            //Creates the map to find player 2
+            p2Map[p2Location.Y] = p2mapLine;
+        }
+
+        /// <summary>
+        /// Determines the target Location that this particular Enemy wants to go to.
+        /// </summary>
+        /// <param name="enemyX"> Enemy center X pos </param>
+        /// <param name="enemyY"> Enemy center y pos </param>
+        /// <returns></returns>
+        private Location DetermineTarget(int enemyX, int enemyY)
+        {
+            // Calculates H scores before hand
+
+            int p1H = ComputeHScore(enemyX, enemyY, p1Location.X, p1Location.Y);
+            int p2H = ComputeHScore(enemyX, enemyY, p1Location.X, p1Location.Y);
+
+            // If P2 is further , then enemy follows P1
+            if (p2H > p1H)
+            {
+                correctMap = p1Map;
+                usePlayer1 = true;
+                return new Location { X = p1Location.X, Y = p1Location.Y };
+            }
+
+            // If P1 is further or they are roughly = distance away, then enemy follows P2
+            correctMap = p2Map;
+            usePlayer1 = false;
+            return new Location { X = p2Location.X, Y = p2Location.Y };
+        }
+
+        /// <summary>
+        /// Determines the closest Point in the game to which the Enemy should move too. 
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns> Returns the Vector2 of the point to which the Enemy should move to!</returns>
+        public Vector2 Following(Enemy e)
+        {
+            var start = new Location { X = (e.Hitbox.Center.X / widthConstant), Y = (e.Hitbox.Center.Y / heightConstant) };
+            var target = DetermineTarget(start.X, start.Y);
             var openList = new List<Location>();
             var closedList = new List<Location>();
             int g = 0;
@@ -86,7 +201,7 @@ namespace Party_Tower_Main
                 if (closedList.FirstOrDefault(l => l.X == target.X && l.Y == target.Y) != null)
                     break;
 
-                var adjacentSquares = GetWalkableAdjacentSquares(current.X, current.Y, map);
+                var adjacentSquares = GetWalkableAdjacentSquares(current.X, current.Y, correctMap);
                 g++;
 
                 foreach (var adjacentSquare in adjacentSquares)
@@ -122,8 +237,37 @@ namespace Party_Tower_Main
                     }
                 }
             }
+
+            float xReturn;  // X value of point this Enemy needs to move to
+            float yReturn;  // Y value of point this Enemy needs to move to
+
+            // The next square of which the enemy must move to.
+            if (closedList.Count > 1)
+            {
+                xReturn = closedList[1].X * widthConstant + (widthConstant / 2);
+                yReturn = closedList[1].Y * heightConstant + (heightConstant / 2);
+                return new Vector2(xReturn, yReturn);
+            }
+
+            // This case occurs if the enemy and player are in the same square.
+
+            // This instance of A* used P2, so travel to the center of Player 2
+            if (!usePlayer1)
+            {
+                return p2Center;
+            }
+
+            // This instance of A* used P1, so travel to the center of Player 1
+            return p1Center;
         }
 
+        /// <summary>
+        /// Returns a list of potental Adjacent Squares that could be the next "current" var when using A*
+        /// </summary>
+        /// <param name="x"> Current X Location Value </param>
+        /// <param name="y"> Current Y Location Value </param>
+        /// <param name="map"></param>
+        /// <returns></returns>
         private List<Location> GetWalkableAdjacentSquares(int x, int y, string[] map)
         {
             var proposedLocations = new List<Location>()
