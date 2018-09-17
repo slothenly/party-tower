6a05c3b0dad861307dd3ac1f18cce44e1ab55f17
Beatrice <Beatrice@DESKTOP-FOGA9V5>
2018-09-17 16:26:49 -0400

    fader added and fully functional

---
+++ Alpha Fade Test/Alpha Fade Test/Game1.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Alpha_Fade_Test
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        float fading_screen_alpha_percent;
+        Rectangle fading_screen_rect;
+        Fader fading_screen_fader;
+        Texture2D fading_screen_texture;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            fading_screen_alpha_percent = 0.0f;
+            fading_screen_rect = new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+            fading_screen_fader = new Fader(30, fading_screen_alpha_percent);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            fading_screen_texture = Content.Load<Texture2D>("fader_color");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            if (!fading_screen_fader.finished)
+            {
+                fading_screen_alpha_percent = fading_screen_fader.change_alpha_percent_positive(fading_screen_alpha_percent);
+            }
+
+            if (fading_screen_fader.finished)
+            {
+                fading_screen_alpha_percent = fading_screen_fader.change_alpha_percent_negative(fading_screen_alpha_percent);
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(fading_screen_texture, fading_screen_rect, Color.White * fading_screen_alpha_percent);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Alpha Fade Test/Alpha Fade Test/fader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Alpha_Fade_Test
+{
+    class Fader
+    {
+        //fields
+        float alpha_percent;
+        float opacity_value;
+        float timeframe;
+        float change_per_frame;
+        public bool finished;
+
+        //methods
+        /// <summary>
+        //  updates every frame changing the opacity a percentage per frame
+        /// </summary>
+        /// <param name="timeframe">amount of frames</param>
+        public Fader(float timeframe_input, float original)
+        {
+            this.timeframe = timeframe_input;
+            change_per_frame = (1 / timeframe);
+            opacity_value = 0.0f;
+
+            finished = false;
+        }
+
+        public float change_alpha_percent_positive(float current)
+        {
+            if (opacity_value < 1)
+            {
+                opacity_value += change_per_frame;
+            }
+            else if (opacity_value > 1)
+            {
+                opacity_value = 1;
+                finished = true;
+            }
+
+            return opacity_value;
+        }
+
+        public float change_alpha_percent_negative(float current)
+        {
+            if (opacity_value > 0)
+            {
+                opacity_value -= change_per_frame;
+            }
+            else if (opacity_value < 0)
+            {
+                opacity_value = 0;
+                finished = false;
+            }
+
+            return opacity_value;
+        }
+    }
+}
